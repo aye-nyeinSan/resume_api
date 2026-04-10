2744b1e4001e63673a62ab83e02c4f7c8ef4ec21
AyeNyeinSan <aye2904@gmail.com>
2026-04-10 19:52:57 +0700

    Update: firestore credential calling

--- config/firestoreDb.py
+++ config/firestoreDb.py
@@ -1,6 +1,6 @@
 from google.cloud import firestore
 
 # Initialize Firestore client
-db = firestore.Client.from_service_account_json('./serviceAccountKey.json')
+db = firestore.Client()
 
 visitor_ref = db.collection('visitors')
